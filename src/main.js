fd52b8f38a72b1c98b338ee1a9e1e130e740313c
Gabriela Aldana <0225806@up.edu.mx>
2022-05-12 08:33:19 -0500

    Claves parte 1
    Cambio de lenguaje de TS a JS y config de Firebase para base de datos

--- src/main.js
+++ src/main.js
@@ -23,9 +23,31 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+
+//Cosas FIREBASE
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBhyqE5yAnLGVyxmGgLjoPgjfrAgRczbTY",
+  authDomain: "listafirebase-62cba.firebaseapp.com",
+  databaseURL: "https://listafirebase-62cba-default-rtdb.firebaseio.com",
+  projectId: "listafirebase-62cba",
+  storageBucket: "listafirebase-62cba.appspot.com",
+  messagingSenderId: "373033327260",
+  appId: "1:373033327260:web:d1ef797d0becce5ed22b59"
+};
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig);
+
+
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
+  .use(router)
+  .use(appFirebase);
   
 router.isReady().then(() => {
   app.mount('#app');
